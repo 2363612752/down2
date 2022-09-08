60b9af36730ef348c6ce7d2c621b6daccf4d9e01
2363612752 <2363612752@qq.com>
2022-09-08 11:03:29 +0800

    init

---
+++ src/views/test/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <a-card :bordered="false">
+    
+  </a-card>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    created(){
+        axios.get('http://192.168.1.3:8081/captchaImage').then(res=>{
+            console.log(res,'============');
+        })
+    }
+}
+</script>
+
+<style>
+
+</style>
